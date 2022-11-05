e5570ae3cbe65fdc488b9c693e91466719fb8322
Yiyun Qin <yiyun.qin@mths.ca>
2022-11-05 22:42:55 +0000

    boardfoot

--- src/index.ts
+++ src/index.ts
@@ -12,28 +12,36 @@ import promptSync from 'prompt-sync'
 const prompt = promptSync()
 
 const area = 144
-let width: number | undefined
-let height: number | undefined
 
-console.log('This program figutes out the length of a wooden board that is 1')
-console.log(' board food in dimension. (1 board foot is 144 inches³ of wood.')
-const widthString = prompt('Enter the width(inch): ')
-const heightString = prompt('Enter the height(inch): ')
-
-const width = parseInt(widthString)
-const height = parseInt(heightString)
-if (isNaN(width) || isNaN(height)) {
-  console.log('Invalid number!')
-} else if (width <= 0 || height <= 0 || width > 144 || height > 144) {
-  console.log('Please enter integers between 1 and 144!')
-} else {
-  const length = BoardFoot(width, height)
-  console.log(`The wood should be ${length} inches long.`)
-}
-
-export function BoardFoot(widthInt: number, heightInt: number): number {
-  const lengthAnswer = area / width / height
+/**
+ * function, calculates the length.
+ *
+ * @param {number} widthInt The width of boardfoot.
+ * @param {number} heightInt The height of boardfoot.
+ * @returns {number} lengthAnswer The final answer for the length.
+ */
+function boardFoot(widthInt: number, heightInt: number): number {
+  let lengthAnswer = area / width / height
+  lengthAnswer = parseFloat(lengthAnswer.toFixed(2))
   return lengthAnswer
 }
 
+console.log(
+  'This program figutes out the length of a wooden board that is 1' +
+    ' board food in dimension. (1 board foot is 144 inches³' +
+    ' of wood.'
+)
+const widthString = prompt('Enter the width(inch): ')
+const heightString = prompt('Enter the height(inch): ')
+
+const width = parseFloat(widthString)
+const height = parseFloat(heightString)
+if (isNaN(width) || isNaN(height)) {
+  console.log('\nInvalid number!')
+} else if (width <= 0 || height <= 0 || width > 144 || height > 144) {
+  console.log('\nPlease enter integers between 1 and 144!')
+} else {
+  const length = boardFoot(width, height)
+  console.log(`\nThe wood should be ${length} inches long.`)
+}
 console.log('\nDone.')
